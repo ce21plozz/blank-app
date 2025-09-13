e9bf09184b5728f14e8b8b45a561ff17d44831d7
ce21plozz <ceplox021@gmail.com>
2025-09-13 13:05:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import random
-st.markdown("<h1 style= 'text-align: center;font-family: serif'>Tempat Duduk Generator</h1>", unsafe_allow_html=True)
-st.markdown("<h4 style= 'text-align: center;font-family: cursive;'>khusus kelas 10 (semua subkelas 10 bisa :D)</h4>", unsafe_allow_html=True)
+st.markdown("<h1 style= 'text-align: center;font-family: cursive'>Tempat Duduk Generator</h1>", unsafe_allow_html=True)
+st.markdown("<h4 style= 'text-align: center;font-family: serif;'>khusus kelas 10 (semua subkelas 10 bisa :D)</h4>", unsafe_allow_html=True)
 def seatgen(subclass):
     list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,29,30,31,32]
     random.shuffle(list)
