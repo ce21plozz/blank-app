7a0a295e36734ed3eddf9ffe37aa08ec3c3f9bfd
ce21plozz <ceplox021@gmail.com>
2025-10-02 16:02:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,49 @@ import streamlit as st
 import random
 import base64
 
+st.markdown("""<style>
+@import url('https://fonts.googleapis.com/css2?family=Amaranth:ital,wght@0,400;0,700;1,400;1,700&family=Anta&family=Convergence&family=Fredoka:wght@550&family=Patrick+Hand&display=swap');
+</style>""", unsafe_allow_html=True)
+st.markdown("""
+<style>
+.fredoka-e {
+  font-family: "Fredoka", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 550;
+  font-style: normal;
+  font-size: 3rem;
+  font-variation-settings:
+    "wdth" 100;
+}
+.patrick-hand-regular {
+  font-family: "Patrick Hand", cursive;
+  font-weight: 400;
+  font-style: normal;
+}
+.anta-regular {
+  font-family: "Anta", sans-serif;
+  font-weight: 400;
+  font-style: normal;
+}
+.amaranth-regular {
+  font-family: "Amaranth", sans-serif;
+  font-weight: 400;
+  font-style: normal;
+}
+.convergence-regular {
+  font-family: "Convergence", sans-serif;
+  font-weight: 400;
+  font-style: normal;
+}
+
+</style>""", unsafe_allow_html=True)
+
 def get_base64(file_path):
     with open(file_path, "rb") as f:
         return base64.b64encode(f.read()).decode()
 
 bg_image = get_base64("newdarkstbg.jpg")
+link_image = get_base64("placeholder_kasual.jpg")
 
 st.markdown(
     f"""
@@ -22,8 +60,12 @@ st.markdown(
     """,unsafe_allow_html=True)
 
 
-st.markdown("<h2 style= 'text-align: center;font-family: Baskerville;color:white'>Tempat Duduk Generator</h2>", unsafe_allow_html=True)
-st.markdown("<h5 style= 'text-align: center;font-family: Courier;color:white'>khusus kelas 10 (semua subkelas 10 bisa :D)</h5>", unsafe_allow_html=True)
+st.markdown("""
+            <div style="background-color:#4377ab;border-radius:8px;padding:10px;">
+                <div class="fredoka-e" style= 'text-align: center;color:white;font-size:2.5rem;'>Tempat Duduk Generator</div>
+                <br style="margin-top:0px;">
+                <div class="anta-regular" style= 'text-align: center;color:white;font-size:1.65rem;'>khusus untuk semua kelas 10</div>
+            </div>""", unsafe_allow_html=True)
 def seatgen(subclass):
     list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,29,30,31,32]
     random.shuffle(list)
@@ -164,16 +206,18 @@ def seatgen(subclass):
     if subclass == "X-6":
         st.markdown("""  
 <br>
-<div style="text-align: center; background-color: black;border-radius:5px">  
-  <span style="  
-    background: linear-gradient(90deg, red, orange, yellow, lime,blue, cyan,violet, pink);  
-    -webkit-background-clip: text;  
-    -webkit-text-fill-color: transparent;  
-    font-size: 0.95rem;  
-  ">  
-    ☝️Senin-Kamis sesuai ini☝️ Jumat bebas!! ✨  
-  </span>  
-</div>  
+<div style="text-align:center;max-width: 100%; margin: auto;">
+    <span style="text-align: center; background-color: black;border-radius:5px">  
+      <span style="  
+        background: linear-gradient(90deg, red, orange, yellow, lime,blue, cyan,violet, pink);  
+        -webkit-background-clip: text;  
+        -webkit-text-fill-color: transparent;  
+        font-size: 0.95rem;  
+      ">  
+        ☝️Senin-Kamis sesuai ini☝️ Jumat bebas!! ✨  
+      </span>  
+    </span>
+</div> 
 """, unsafe_allow_html=True)
     st.markdown("<hr style= 'height:2px;background-color:white';border-radius:20px;margin: 0px>", unsafe_allow_html=True)
     st.markdown("""
@@ -185,7 +229,7 @@ def seatgen(subclass):
             }
             </style>
         """, unsafe_allow_html=True)
-    st.markdown(f"""<div style='background-color:rgba(0,175,0,0.8);opacity:0.9;border-radius:20px;text-align:left;padding:13px;
+    st.markdown(f"""<div class="amaranth-regular" style='background-color:rgba(0,175,0,0.8);opacity:0.9;border-radius:20px;text-align:left;padding:13px;
                     color: #000000'><i>Pengacakan Berhasil!! 🥳🥳🥳<br>(kamu dah rolling {st.session_state.count} kali)</i></div>""", unsafe_allow_html=True)
 
 st.markdown(
@@ -201,8 +245,8 @@ st.markdown(
 
 st.markdown(
     """
-    <span style = "color:white;margin: 0;font-family:Palatino;font-size:0.85rem">
-    Mau rolling tempat duduk di kelas berapa? 🔢
+    <span class="patrick-hand-regular" style = "color:white;margin: 0;font-size:1.1rem">
+    Rolling tempat duduk kelas berapa? 🔢
     </span>
     """,
     unsafe_allow_html=True
@@ -215,10 +259,10 @@ if st.button("Lakuin Rolling tempat duduk! 🎲"):
         seatgen(subclass)
         st.session_state.count += 1
     elif subclass == 'Kosong':
-        st.markdown("""<div style='background-color:rgba(255,0,0,0.8);opacity:0.9;border-radius:30px;text-align:left;padding:13px;
+        st.markdown("""<div class="amaranth-regular" style='background-color:rgba(255,0,0,0.8);opacity:0.9;border-radius:30px;text-align:left;padding:13px;
                     color: #000000'><i>Pilih kelas dulu lah kocak... 😐</i></div>""", unsafe_allow_html=True)
 else:
-    st.markdown("""<div style='background-color:rgba(242,210,0,0.8);opacity:0.9;border-radius:30px;text-align:left;padding:13px;
+    st.markdown("""<div class="amaranth-regular" style='background-color:rgba(242,210,0,0.8);opacity:0.9;border-radius:30px;text-align:left;padding:13px;
                     color: #000000'><i>Pilih Kelasmu! 📚</i></div>""", unsafe_allow_html=True)
 
 st.markdown("""
@@ -228,3 +272,27 @@ st.markdown("""
         <span style='color:lime;text-shadow:-2px -2px 0 blue;font-weight:bold;font-size:0.85rem'>1.) Gw yg namanya cuma sekata</span> <span style='color:orange;text-shadow:-2px -2px 0 #994708;font-weight:bold;font-size:0.85rem'>(Pembuat Projek)</span>
         <br>
         <span style='color:lime;text-shadow:-2px -2px 0 blue; font-weight:bold;font-size:0.85rem'>2.) Si tukang elektronik/komputer itu</span> <span style='color:orange;font-weight:bold;text-shadow:-2px -2px 0 #994708;font-size:0.85rem'>(Bugfixer)</span>""", unsafe_allow_html=True)
+st.markdown(f"""
+            <br>
+            <div style="text-align:center;font-size:1.6rem;background-color:#4377ab;border-radius:8px;padding:10px;">
+                <span style='color:white;margin:0px;text-shadow:-2px -2px 0 red;font-weight:bold'> 🛠️🎨 EXTRA 🎨🛠️ </span>
+            </div>
+            <br>
+            <div class="convergence-regular" style='color:yellow;text-shadow:-3px -3px 0 #7b00ff;font-weight:bold;font-size:1.2rem;text-align:center'>Kelompok Generator</div> 
+            <div style="text-align:center">
+                <span class="convergence-regular" style='color:#02eba9;font-weight:bold;text-shadow:-3px -3px 0 #254e94;font-size:0.9rem'>(Jumlah cowo & cewe tiap kelompok yang dibuat langsung adil, pencet gambar di bawah untuk nyoba)</span>
+            </div>
+            
+            <br>
+            <div style="text-align:center">
+                <a href="https://kelompok.streamlit.app/">
+                    <img src="data:image/jpg;base64,{link_image}"
+                    alt = "placeholder"
+                    target = "_blank"
+                    style="border-radius:20px;
+                    cursor:pointer;
+                    width:250px;
+                    border: 5px solid black;
+                    ">               
+                </a>
+            </div>""", unsafe_allow_html=True)
